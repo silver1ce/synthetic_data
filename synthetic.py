b9361ab94303594e6472032c18b6a3c52687b14e
Jahangir Alam <jahangir_alam_99@hotmail.com>
2023-11-13 12:32:53 +0200

    test

--- synthetic.py
+++ synthetic.py
@@ -1,79 +1,92 @@
-import numpy as np
 import pandas as pd
-from tensorflow.keras.layers import Input, Dense, Lambda
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Dense, LeakyReLU
 from tensorflow.keras.models import Model
-from tensorflow.keras.losses import mse
-from tensorflow.keras import backend as K
+from tensorflow.keras.optimizers import Adam
+from sklearn.preprocessing import MinMaxScaler
 
-# Sample original dataset with an object column
-# Replace this with your actual data loading code
-original_data = pd.DataFrame({'object_col': ['Category A', 'Category B', 'Category C'] * 333})
+# Load the sample CSV dataset (replace 'sample.csv' with your dataset file)
+df = pd.read_csv('sample.csv')
 
-# Define a VAE architecture for generating object data
-def build_object_vae(input_dim, latent_dim):
-    input_layer = Input(shape=(input_dim,))
-    
-    # Encoder
-    encoded = Dense(256, activation='relu')(input_layer)
-    encoded = Dense(128, activation='relu')(encoded)
-    
-    # Latent space mean and log variance
-    z_mean = Dense(latent_dim)(encoded)
-    z_log_var = Dense(latent_dim)(encoded)
-    
-    # Reparameterization trick
-    def sampling(args):
-        z_mean, z_log_var = args
-        epsilon = K.random_normal(shape=(K.shape(z_mean)[0], latent_dim), mean=0., stddev=1.0)
-        return z_mean + K.exp(0.5 * z_log_var) * epsilon
-    
-    z = Lambda(sampling)([z_mean, z_log_var])
-    
-    # Decoder
-    decoded = Dense(128, activation='relu')(z)
-    decoded = Dense(256, activation='relu')(decoded)
-    decoded = Dense(input_dim, activation='softmax')(decoded)  # Softmax activation for object data
-    
-    # VAE model
-    vae = Model(input_layer, decoded)
-    
-    # VAE loss function
-    reconstruction_loss = mse(input_layer, decoded)
-    reconstruction_loss *= input_dim  # Adjust reconstruction loss
-    kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
-    kl_loss = K.sum(kl_loss, axis=-1)
-    kl_loss *= -0.5
-    vae_loss = K.mean(reconstruction_loss + kl_loss)
-    
-    vae.add_loss(vae_loss)
-    vae.compile(optimizer='adam')
-    
-    return vae
+# Preprocess the data (handle missing values, encode categorical variables, normalize numeric data)
+# You can use libraries like scikit-learn for preprocessing
+scaler = MinMaxScaler()
+df[df.columns] = scaler.fit_transform(df[df.columns])
 
-# Define a function to generate synthetic object data
-def generate_synthetic_object_data(vae, num_samples=1000):
-    latent_dim = vae.layers[5].output_shape[1]  # Get the latent dimension
-    latent_samples = np.random.normal(0, 1, (num_samples, latent_dim))
-    synthetic_data = vae.predict(latent_samples)
-    return synthetic_data
+# Define the GAN architecture
+def build_generator(latent_dim, num_features):
+    input_noise = Input(shape=(latent_dim,))
+    x = Dense(128, activation='relu')(input_noise)
+    x = Dense(256, activation='relu')(x)
+    x = Dense(num_features, activation='sigmoid')(x)
+    generator = Model(inputs=input_noise, outputs=x)
+    return generator
 
-# Example usage
-if __name__ == "__main__":
-    num_samples = 1000  # Number of synthetic samples to generate
-    
-    # Build and train the object VAE
-    object_input_dim = len(original_data['object_col'].unique())  # Dimension for one-hot encoding
-    object_vae = build_object_vae(object_input_dim, latent_dim=10)
-    
-    # Train the object VAE on object data (replace with your data loading and preprocessing)
-    object_real_data = original_data['object_col'].values
-    
-    # One-hot encode the object data
-    object_real_data_encoded = pd.get_dummies(object_real_data)
-    
-    # Training code for object VAE goes here
-    
-    # Generate synthetic object data
-    synthetic_object_data = generate_synthetic_object_data(object_vae, num_samples=num_samples)
+def build_discriminator(num_features):
+    input_data = Input(shape=(num_features,))
+    x = Dense(256, activation=LeakyReLU(alpha=0.2))(input_data)
+    x = Dense(128, activation=LeakyReLU(alpha=0.2))(x)
+    x = Dense(1, activation='sigmoid')(x)
+    discriminator = Model(inputs=input_data, outputs=x)
+    return discriminator
 
-    # Now, 'synthetic_object_data' contains synthetic object data.
+def build_gan(generator, discriminator):
+    discriminator.trainable = False
+    input_noise = Input(shape=(latent_dim,))
+    generated_data = generator(input_noise)
+    validity = discriminator(generated_data)
+    gan = Model(inputs=input_noise, outputs=validity)
+    return gan
+
+# Hyperparameters (adjust as needed)
+latent_dim = 32
+num_features = df.shape[1]  # Number of columns in your dataset
+batch_size = 64
+epochs = 10000
+
+# Build and compile the models
+generator = build_generator(latent_dim, num_features)
+discriminator = build_discriminator(num_features)
+gan = build_gan(generator, discriminator)
+
+discriminator.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.0002), metrics=['accuracy'])
+gan.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.0002))
+
+# Training loop
+for epoch in range(epochs):
+    # Sample random noise as input
+    noise = np.random.randn(batch_size, latent_dim)
+    
+    # Generate synthetic data
+    synthetic_data = generator.predict(noise)
+    
+    # Train discriminator on real data
+    real_labels = np.ones((batch_size, 1))
+    d_loss_real = discriminator.train_on_batch(df.sample(batch_size).values, real_labels)
+    
+    # Train discriminator on generated data
+    fake_labels = np.zeros((batch_size, 1))
+    d_loss_fake = discriminator.train_on_batch(synthetic_data, fake_labels)
+    
+    # Calculate discriminator loss
+    d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+    
+    # Train generator
+    noise = np.random.randn(batch_size, latent_dim)
+    valid_labels = np.ones((batch_size, 1))
+    g_loss = gan.train_on_batch(noise, valid_labels)
+    
+    # Print progress
+    if epoch % 100 == 0:
+        print(f"Epoch {epoch}, D Loss: {d_loss[0]}, G Loss: {g_loss}")
+
+# Generate synthetic data
+num_synthetic_samples = 1000
+synthetic_noise = np.random.randn(num_synthetic_samples, latent_dim)
+synthetic_data = generator.predict(synthetic_noise)
+
+# Convert synthetic data to a DataFrame (assuming column names are preserved)
+synthetic_df = pd.DataFrame(data=synthetic_data, columns=df.columns)
+
+# You can now use 'synthetic_df' as your synthetic dataset
