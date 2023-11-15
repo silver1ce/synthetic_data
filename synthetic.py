562ac3b394861fd077dc63b1cff9c6f60f415b1b
Jahangir Alam <jahangir_alam_99@hotmail.com>
2023-11-15 20:30:07 +0200

    test

--- synthetic.py
+++ synthetic.py
@@ -1,92 +1,96 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Dense, LeakyReLU
+from tensorflow.keras.layers import Input, Dense, BatchNormalization, LeakyReLU, Dropout
 from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
 
-# Load the sample CSV dataset (replace 'sample.csv' with your dataset file)
-df = pd.read_csv('sample.csv')
+# Load your real dataset (replace this with your actual data loading code)
+real_data = pd.read_csv('your_real_data.csv')
 
-# Preprocess the data (handle missing values, encode categorical variables, normalize numeric data)
-# You can use libraries like scikit-learn for preprocessing
+# Preprocess your data
+# Split the data into object, datetime, and numeric features
+object_data = real_data.select_dtypes(include=['object'])
+datetime_data = real_data.select_dtypes(include=['datetime64'])
+numeric_data = real_data.select_dtypes(include=['float64', 'int64'])
+
+# Normalize numeric data (you can choose other scaling methods as well)
 scaler = MinMaxScaler()
-df[df.columns] = scaler.fit_transform(df[df.columns])
+scaled_numeric_data = scaler.fit_transform(numeric_data)
 
-# Define the GAN architecture
-def build_generator(latent_dim, num_features):
-    input_noise = Input(shape=(latent_dim,))
-    x = Dense(128, activation='relu')(input_noise)
-    x = Dense(256, activation='relu')(x)
-    x = Dense(num_features, activation='sigmoid')(x)
-    generator = Model(inputs=input_noise, outputs=x)
-    return generator
+# Define GAN architecture
+def build_generator(latent_dim, output_dim):
+    model = tf.keras.Sequential()
+    model.add(Dense(128, input_dim=latent_dim))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Dense(256))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Dense(output_dim, activation='sigmoid'))
+    return model
 
-def build_discriminator(num_features):
-    input_data = Input(shape=(num_features,))
-    x = Dense(256, activation=LeakyReLU(alpha=0.2))(input_data)
-    x = Dense(128, activation=LeakyReLU(alpha=0.2))(x)
-    x = Dense(1, activation='sigmoid')(x)
-    discriminator = Model(inputs=input_data, outputs=x)
-    return discriminator
+def build_discriminator(input_dim):
+    model = tf.keras.Sequential()
+    model.add(Dense(256, input_dim=input_dim))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dropout(0.25))
+    model.add(Dense(128))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dropout(0.25))
+    model.add(Dense(1, activation='sigmoid'))
+    return model
 
-def build_gan(generator, discriminator):
-    discriminator.trainable = False
-    input_noise = Input(shape=(latent_dim,))
-    generated_data = generator(input_noise)
-    validity = discriminator(generated_data)
-    gan = Model(inputs=input_noise, outputs=validity)
-    return gan
+# Define GAN hyperparameters
+latent_dim = 100  # Size of the generator's input noise vector
+output_dim = scaled_numeric_data.shape[1]  # Size of the output data
 
-# Hyperparameters (adjust as needed)
-latent_dim = 32
-num_features = df.shape[1]  # Number of columns in your dataset
-batch_size = 64
-epochs = 10000
+# Build and compile the discriminator
+discriminator = build_discriminator(output_dim)
+discriminator.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# Build and compile the models
-generator = build_generator(latent_dim, num_features)
-discriminator = build_discriminator(num_features)
-gan = build_gan(generator, discriminator)
+# Build the generator
+generator = build_generator(latent_dim, output_dim)
 
-discriminator.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.0002), metrics=['accuracy'])
-gan.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.0002))
+# The GAN combines the generator and discriminator
+discriminator.trainable = False
+gan_input = Input(shape=(latent_dim,))
+x = generator(gan_input)
+gan_output = discriminator(x)
+gan = Model(gan_input, gan_output)
+gan.compile(loss='binary_crossentropy', optimizer='adam')
 
 # Training loop
+# ...
+batch_size = 64
+epochs = 10000  # Adjust as needed
+save_interval = 1000  # Adjust as needed
+
 for epoch in range(epochs):
-    # Sample random noise as input
-    noise = np.random.randn(batch_size, latent_dim)
-    
+    # Train discriminator with real data
+    real_data_batch = scaled_numeric_data[np.random.randint(0, scaled_numeric_data.shape[0], batch_size)]
+    labels_real = np.ones((batch_size, 1))
+
     # Generate synthetic data
-    synthetic_data = generator.predict(noise)
-    
-    # Train discriminator on real data
-    real_labels = np.ones((batch_size, 1))
-    d_loss_real = discriminator.train_on_batch(df.sample(batch_size).values, real_labels)
-    
-    # Train discriminator on generated data
-    fake_labels = np.zeros((batch_size, 1))
-    d_loss_fake = discriminator.train_on_batch(synthetic_data, fake_labels)
-    
-    # Calculate discriminator loss
-    d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
-    
+    noise = np.random.normal(0, 1, (batch_size, latent_dim))
+    generated_data_batch = generator.predict(noise)
+    labels_fake = np.zeros((batch_size, 1))
+
+    # Train discriminator
+    d_loss_real = discriminator.train_on_batch(real_data_batch, labels_real)
+    d_loss_fake = discriminator.train_on_batch(generated_data_batch, labels_fake)
+
     # Train generator
-    noise = np.random.randn(batch_size, latent_dim)
-    valid_labels = np.ones((batch_size, 1))
-    g_loss = gan.train_on_batch(noise, valid_labels)
-    
+    noise = np.random.normal(0, 1, (batch_size, latent_dim))
+    labels_gan = np.ones((batch_size, 1))
+    g_loss = gan.train_on_batch(noise, labels_gan)
+
     # Print progress
-    if epoch % 100 == 0:
-        print(f"Epoch {epoch}, D Loss: {d_loss[0]}, G Loss: {g_loss}")
+    if epoch % save_interval == 0:
+        print(f"Epoch {epoch}, D Loss Real: {d_loss_real[0]}, D Loss Fake: {d_loss_fake[0]}, G Loss: {g_loss}")
 
-# Generate synthetic data
-num_synthetic_samples = 1000
-synthetic_noise = np.random.randn(num_synthetic_samples, latent_dim)
-synthetic_data = generator.predict(synthetic_noise)
-
-# Convert synthetic data to a DataFrame (assuming column names are preserved)
-synthetic_df = pd.DataFrame(data=synthetic_data, columns=df.columns)
-
-# You can now use 'synthetic_df' as your synthetic dataset
+        # Save generated data (you can adjust the saving mechanism)
+        generated_data = scaler.inverse_transform(generator.predict(noise))
+        synthetic_df = pd.DataFrame(data=generated_data, columns=numeric_data.columns)
+        synthetic_df
