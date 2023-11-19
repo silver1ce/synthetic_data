8741e07a5dc5feda0ac526ae94dd081ec8d892ef
Jahangir Alam <jahangir_alam_99@hotmail.com>
2023-11-19 12:15:44 +0200

    test

--- synthetic.py
+++ synthetic.py
@@ -1,178 +1,68 @@
 import numpy as np
 import pandas as pd
-import tensorflow as tf
-from sklearn.preprocessing import MinMaxScaler, LabelEncoder, OneHotEncoder
-from tensorflow.keras.layers import Input, Dense, BatchNormalization, LeakyReLU, Embedding, LSTM, Flatten
+from tensorflow.keras.layers import Input, Dense, Embedding, Flatten, Concatenate
 from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
 
-# Load your real dataset (replace this with your actual data loading code)
-real_data = pd.read_csv('your_real_data.csv')
+# Generate some example data with categorical columns
+# Replace this with your actual dataset
+original_data = original_data
 
-# Define GAN architecture for numeric data
-def build_numeric_generator(latent_dim, output_dim):
-    model = tf.keras.Sequential()
-    model.add(Dense(128, input_dim=latent_dim))
-    model.add(LeakyReLU(alpha=0.2))
-    model.add(BatchNormalization(momentum=0.8))
-    model.add(Dense(256))
-    model.add(LeakyReLU(alpha=0.2))
-    model.add(BatchNormalization(momentum=0.8))
-    model.add(Dense(output_dim, activation='sigmoid'))
-    return model
+# Define the GAN-like generator and discriminator networks
+def build_generator(input_dim, output_dim):
+    inputs = Input(shape=(input_dim,))
+    embeddings = []
+    for i in range(input_dim):
+        emb = Embedding(output_dim=output_dim[i], input_dim=output_dim[i])(inputs[:, i:i+1])
+        embeddings.append(emb)
+    merged = Concatenate()(embeddings)
+    output = Flatten()(merged)
+    return Model(inputs, output)
 
-def build_numeric_discriminator(input_dim):
-    model = tf.keras.Sequential()
-    model.add(Dense(256, input_dim=input_dim))
-    model.add(LeakyReLU(alpha=0.2))
-    model.add(Dense(128))
-    model.add(LeakyReLU(alpha=0.2))
-    model.add(Dense(1, activation='sigmoid'))
-    return model
+def build_discriminator(input_dim):
+    inputs = Input(shape=(input_dim,))
+    x = Dense(64, activation='relu')(inputs)
+    x = Dense(32, activation='relu')(x)
+    output = Dense(1, activation='sigmoid')(x)
+    return Model(inputs, output)
 
-# Define GAN architecture for object data
-def build_object_generator(latent_dim, output_dim):
-    model = tf.keras.Sequential()
-    model.add(Dense(128, input_dim=latent_dim))
-    model.add(LeakyReLU(alpha=0.2))
-    model.add(BatchNormalization(momentum=0.8))
-    model.add(Dense(256))
-    model.add(LeakyReLU(alpha=0.2))
-    model.add(BatchNormalization(momentum=0.8))
-    model.add(Dense(output_dim, activation='softmax'))  # Use softmax for object data
-    return model
+# Set parameters
+input_dim = len(original_data.columns)  # Number of categorical columns
+output_dim = [len(original_data[column].unique()) for column in original_data.columns]
 
-def build_object_discriminator(input_dim, num_categories):
-    model = tf.keras.Sequential()
-    model.add(Dense(256, input_dim=input_dim))
-    model.add(LeakyReLU(alpha=0.2))
-    model.add(Dense(128))
-    model.add(LeakyReLU(alpha=0.2))
-    model.add(Dense(num_categories, activation='softmax'))  # Use softmax for object data
-    return model
+# Build and compile the generator and discriminator
+generator = build_generator(input_dim, output_dim)
+discriminator = build_discriminator(input_dim)
 
-# Define GAN architecture for datetime data
-def build_datetime_generator(latent_dim, output_dim):
-    model = tf.keras.Sequential()
-    model.add(Dense(128, input_dim=latent_dim))
-    model.add(LeakyReLU(alpha=0.2))
-    model.add(BatchNormalization(momentum=0.8))
-    model.add(Dense(256))
-    model.add(LeakyReLU(alpha=0.2))
-    model.add(BatchNormalization(momentum=0.8))
-    model.add(Dense(output_dim, activation='sigmoid'))
-    return model
+discriminator.compile(loss='binary_crossentropy', optimizer=Adam(0.0002, 0.5), metrics=['accuracy'])
+discriminator.trainable = False
 
-def build_datetime_discriminator(input_dim):
-    model = tf.keras.Sequential()
-    model.add(Dense(256, input_dim=input_dim))
-    model.add(LeakyReLU(alpha=0.2))
-    model.add(Dense(128))
-    model.add(LeakyReLU(alpha=0.2))
-    model.add(Dense(1, activation='sigmoid'))
-    return model
-
-# Define GAN hyperparameters
-numeric_latent_dim = 100  # Size of the generator's input noise vector for numeric data
-object_latent_dim = 50   # Size of the generator's input noise vector for object data
-datetime_latent_dim = 10 # Size of the generator's input noise vector for datetime data
-output_dim_numeric = real_data.select_dtypes(include=['float64', 'int64']).shape[1]
-categorical_columns = real_data.select_dtypes(include=['object']).columns.tolist()
-output_dim_object = len(categorical_columns)
-output_dim_datetime = real_data.select_dtypes(include=['datetime64']).shape[1]
-
-# Preprocess numeric data
-numeric_data = real_data.select_dtypes(include=['float64', 'int64'])
-scaler = MinMaxScaler()
-scaled_numeric_data = scaler.fit_transform(numeric_data)
-
-# Preprocess object data
-categorical_data = real_data.select_dtypes(include=['object'])
-categorical_data_encoded = pd.get_dummies(categorical_data, columns=categorical_columns, drop_first=True)
-
-# Preprocess datetime data (you may need to extract relevant features)
-datetime_data = real_data.select_dtypes(include=['datetime64'])
-
-# Define discriminator models for each data type
-numeric_discriminator = build_numeric_discriminator(output_dim_numeric)
-object_discriminator = build_object_discriminator(output_dim_object, num_categories=2)
-datetime_discriminator = build_datetime_discriminator(output_dim_datetime)
-
-# Define generator models for each data type
-numeric_generator = build_numeric_generator(numeric_latent_dim, output_dim_numeric)
-object_generator = build_object_generator(object_latent_dim, output_dim_object)
-datetime_generator = build_datetime_generator(datetime_latent_dim, output_dim_datetime)
-
-# Compile discriminator models
-numeric_discriminator.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-object_discriminator.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-datetime_discriminator.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-# Compile generator models
-numeric_generator.compile(loss='binary_crossentropy', optimizer='adam')
-object_generator.compile(loss='categorical_crossentropy', optimizer='adam')
-datetime_generator.compile(loss='binary_crossentropy', optimizer='adam')
-
-# Training loop (similar to previous code examples)
-# ... (previous code for building models and preprocessing data)
+gan_input = Input(shape=(input_dim,))
+x = generator(gan_input)
+gan_output = discriminator(x)
+gan = Model(gan_input, gan_output)
+gan.compile(loss='binary_crossentropy', optimizer=Adam(0.0002, 0.5))
 
 # Training loop
-epochs = 10000  # Adjust as needed
-batch_size = 64
+batch_size = 32
+epochs = 10000
 
 for epoch in range(epochs):
-    # Train numeric data
-    noise_numeric = np.random.normal(0, 1, (batch_size, numeric_latent_dim))
-    generated_numeric_data = numeric_generator.predict(noise_numeric)
-    real_numeric_data = scaled_numeric_data[np.random.randint(0, scaled_numeric_data.shape[0], batch_size)]
-    
-    numeric_discriminator.train_on_batch(generated_numeric_data, np.zeros((batch_size, 1)))
-    numeric_discriminator.train_on_batch(real_numeric_data, np.ones((batch_size, 1)))
-    
-    # Train object data
-    for column in categorical_columns:
-        noise_object = np.random.randint(0, 2, size=(batch_size, object_latent_dim))
-        generated_object_data = object_generator.predict(noise_object)
-        real_object_data = categorical_data_encoded.sample(batch_size)
-        
-        object_discriminator.train_on_batch(generated_object_data, np.zeros((batch_size, 2)))
-        object_discriminator.train_on_batch(real_object_data, np.ones((batch_size, 2)))
-    
-    # Train datetime data
-    noise_datetime = np.random.normal(0, 1, (batch_size, datetime_latent_dim))
-    generated_datetime_data = datetime_generator.predict(noise_datetime)
-    real_datetime_data = datetime_data.sample(batch_size)
-    
-    datetime_discriminator.train_on_batch(generated_datetime_data, np.zeros((batch_size, 1)))
-    datetime_discriminator.train_on_batch(real_datetime_data, np.ones((batch_size, 1)))
-    
-    # Print progress or save generated data as needed
-    
-    if epoch % save_interval == 0:
-        print(f"Epoch {epoch}, ...")
+    noise = np.random.randint(0, np.max(output_dim), size=(batch_size, input_dim))
+    generated_data = generator.predict(noise)
+    real_data = original_data.sample(batch_size)
+    x_combined = np.concatenate([real_data, generated_data])
+    y_combined = np.concatenate([np.ones((batch_size, 1)), np.zeros((batch_size, 1))])
+    d_loss = discriminator.train_on_batch(x_combined, y_combined)
 
-# Generate synthetic data for each data type (as shown in the previous code)
-# ...
+    noise = np.random.randint(0, np.max(output_dim), size=(batch_size, input_dim))
+    y_mislabeled = np.ones((batch_size, 1))
+    g_loss = gan.train_on_batch(noise, y_mislabeled)
 
+    if epoch % 100 == 0:
+        print(f"Epoch {epoch}: D Loss: {d_loss[0]}, G Loss: {g_loss}")
 
-# Generate synthetic data for each data type
-num_samples = 1000  # Adjust the number of synthetic samples you want
-synthetic_numeric = numeric_generator.predict(np.random.normal(0, 1, (num_samples, numeric_latent_dim)))
-synthetic_object = object_generator.predict(np.random.normal(0, 1, (num_samples, object_latent_dim)))
-synthetic_datetime = datetime_generator.predict(np.random.normal(0, 1, (num_samples, datetime_latent_dim)))
-
-# Convert synthetic numeric data back to its original scale
-synthetic_numeric = scaler.inverse_transform(synthetic_numeric)
-
-# Convert synthetic object data back to original categories
-for column in categorical_columns:
-    # Inverse transformation for one-hot encoded categorical data
-    inverse_encoded_data = pd.DataFrame(data=synthetic_object[column], columns=categorical_data_encoded.columns)
-    synthetic_object[column] = inverse_encoded_data.idxmax(axis=1)
-
-# Create a DataFrame for each data type
-synthetic_numeric_df = pd.DataFrame(data=synthetic_numeric, columns=numeric_data.columns)
-synthetic_object_df = pd.DataFrame(data=synthetic_object, columns=categorical_columns)
-synthetic_datetime_df = pd.DataFrame(data=synthetic_datetime, columns=datetime_data.columns)
-
-# Concatenate all the synthetic DataFrames horizontally to combine them
-synthetic_data = pd.concat([synthetic_numeric_df, synthetic_object_df, synthetic_datetime_df], axis=1)
+# Generate synthetic categorical data
+num_samples = 1000
+synthetic_data = generator.predict(np.random.randint(0, np.max(output_dim), size=(num_samples, input_dim)))
+synthetic_data = pd.DataFrame(synthetic_data, columns=original_data.columns)
