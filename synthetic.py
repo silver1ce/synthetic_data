987902bdd559f3f0ad2ff7bd96d26d9d9343bfec
Jahangir Alam <jahangir_alam_99@hotmail.com>
2023-11-23 08:47:40 +0200

    test

--- synthetic.py
+++ synthetic.py
@@ -1,68 +1,90 @@
 import numpy as np
 import pandas as pd
-from tensorflow.keras.layers import Input, Dense, Embedding, Flatten, Concatenate
-from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+from torchvision import datasets, transforms
 
-# Generate some example data with categorical columns
-# Replace this with your actual dataset
-original_data = original_data
+# Load and preprocess the example dataset (assuming it's in CSV format)
+example_file = 'example.csv'
+data = pd.read_csv(example_file)
+# Preprocess the data as needed (e.g., scaling, normalization)
 
-# Define the GAN-like generator and discriminator networks
-def build_generator(input_dim, output_dim):
-    inputs = Input(shape=(input_dim,))
-    embeddings = []
-    for i in range(input_dim):
-        emb = Embedding(output_dim=output_dim[i], input_dim=output_dim[i])(inputs[:, i:i+1])
-        embeddings.append(emb)
-    merged = Concatenate()(embeddings)
-    output = Flatten()(merged)
-    return Model(inputs, output)
+# Convert the preprocessed data to a PyTorch tensor
+data_tensor = torch.tensor(data.values, dtype=torch.float32)
 
-def build_discriminator(input_dim):
-    inputs = Input(shape=(input_dim,))
-    x = Dense(64, activation='relu')(inputs)
-    x = Dense(32, activation='relu')(x)
-    output = Dense(1, activation='sigmoid')(x)
-    return Model(inputs, output)
+# Define a Variational Autoencoder (VAE) model
+class VAE(nn.Module):
+    def __init__(self, input_dim, latent_dim):
+        super(VAE, self).__init__()
+        # Define the encoder and decoder layers
+        # Customize the architecture as needed based on your data
+        self.encoder = nn.Sequential(
+            nn.Linear(input_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, latent_dim * 2)  # The last layer outputs mean and log-variance
+        )
+        self.decoder = nn.Sequential(
+            nn.Linear(latent_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, input_dim),
+            nn.Sigmoid()  # Sigmoid for bounded data (0 to 1)
+        )
 
-# Set parameters
-input_dim = len(original_data.columns)  # Number of categorical columns
-output_dim = [len(original_data[column].unique()) for column in original_data.columns]
+    def reparameterize(self, mu, log_var):
+        # Reparameterization trick to sample from the latent space
+        std = torch.exp(0.5 * log_var)
+        eps = torch.randn_like(std)
+        return mu + eps * std
 
-# Build and compile the generator and discriminator
-generator = build_generator(input_dim, output_dim)
-discriminator = build_discriminator(input_dim)
+    def forward(self, x):
+        # Forward pass through the encoder and decoder
+        enc = self.encoder(x)
+        mu, log_var = enc[:, :latent_dim], enc[:, latent_dim:]
+        z = self.reparameterize(mu, log_var)
+        recon = self.decoder(z)
+        return recon, mu, log_var
 
-discriminator.compile(loss='binary_crossentropy', optimizer=Adam(0.0002, 0.5), metrics=['accuracy'])
-discriminator.trainable = False
+# Define hyperparameters
+input_dim = data_tensor.shape[1]
+latent_dim = 10
+batch_size = 64
+epochs = 100
+learning_rate = 0.001
 
-gan_input = Input(shape=(input_dim,))
-x = generator(gan_input)
-gan_output = discriminator(x)
-gan = Model(gan_input, gan_output)
-gan.compile(loss='binary_crossentropy', optimizer=Adam(0.0002, 0.5))
+# Create a DataLoader for the dataset
+dataset = TensorDataset(data_tensor)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+# Create the VAE model and optimizer
+model = VAE(input_dim, latent_dim)
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
 # Training loop
-batch_size = 32
-epochs = 10000
-
 for epoch in range(epochs):
-    noise = np.random.randint(0, np.max(output_dim), size=(batch_size, input_dim))
-    generated_data = generator.predict(noise)
-    real_data = original_data.sample(batch_size)
-    x_combined = np.concatenate([real_data, generated_data])
-    y_combined = np.concatenate([np.ones((batch_size, 1)), np.zeros((batch_size, 1))])
-    d_loss = discriminator.train_on_batch(x_combined, y_combined)
+    for batch_data in dataloader:
+        optimizer.zero_grad()
+        recon_data, mu, log_var = model(batch_data[0])
+        # Define a suitable reconstruction loss (e.g., mean squared error)
+        reconstruction_loss = nn.MSELoss()(recon_data, batch_data[0])
+        # Define a suitable regularization loss term (e.g., KL divergence)
+        kl_divergence = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
+        total_loss = reconstruction_loss + kl_divergence
+        total_loss.backward()
+        optimizer.step()
 
-    noise = np.random.randint(0, np.max(output_dim), size=(batch_size, input_dim))
-    y_mislabeled = np.ones((batch_size, 1))
-    g_loss = gan.train_on_batch(noise, y_mislabeled)
-
-    if epoch % 100 == 0:
-        print(f"Epoch {epoch}: D Loss: {d_loss[0]}, G Loss: {g_loss}")
-
-# Generate synthetic categorical data
+# Generate synthetic data samples
 num_samples = 1000
-synthetic_data = generator.predict(np.random.randint(0, np.max(output_dim), size=(num_samples, input_dim)))
-synthetic_data = pd.DataFrame(synthetic_data, columns=original_data.columns)
+with torch.no_grad():
+    z_samples = torch.randn(num_samples, latent_dim)
+    synthetic_samples = model.decoder(z_samples).numpy()
+
+# Create a DataFrame for the synthetic data
+synthetic_df = pd.DataFrame(synthetic_samples, columns=data.columns)
+
+# Save the synthetic data to a CSV file
+synthetic_file = 'synthetic_data.csv'
+synthetic_df.to_csv(synthetic_file, index=False)
+
+print(f"Synthetic data saved to {synthetic_file}")
